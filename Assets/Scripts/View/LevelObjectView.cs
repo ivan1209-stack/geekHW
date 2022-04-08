8baeda267c58cedc85c33fa65a5bf2fc0160ea1b
Ivan Bottomweb <darovskoy.ivan@yandex.ru>
2022-04-09 00:29:24 +0300

    first homework

---
+++ Assets/Scripts/View/LevelObjectView.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Platformer
+{
+    public class LevelObjectView : MonoBehaviour
+    {
+        public Transform Transform;
+        public SpriteRenderer SpriteRenderer;
+        public Collider2D Collider;
+        public Rigidbody2D RigidBody;
+    }
+}
